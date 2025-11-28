a5717cc8dddccc3428fb5d759e5a3753e01b8a9e
joeswagson <uranousbridge@gmail.com>
2025-11-28 00:57:53 -0800

    Camera test script

--- gittest.py
+++ gittest.py
@@ -1,1 +1,15 @@
-print("eyyyy im on a nightstand im nighstanding it")
+from picamera2 import Picamera2
+from time import sleep
+
+picam = Picamera2()
+
+config = picam.create_still_configuration()
+picam.configure(config)
+
+picam.start()
+sleep(1)
+
+picam.capture_file("output.jpg")
+picam.stop()
+
+print("saved image")
